b8bfcb10fd9a9cdab8bd93c23c661b5960e4d3cd
Mhand <amd.maous@gmail.com>
2022-02-28 18:54:33 +0100

    Changed  based url for blobs

--- index.js
+++ index.js
@@ -1,18 +1,17 @@
 'use strict';
 
 // const uuid = require('uuid');
-const client = require('./client');
+const { IMAGE_BASE_URL, client } = require('./client');
 const DetectFaceExtract = require('./DetectFaceExtract');
 
 // global values and helper functions
-const image_base_url =
-  'https://test1azure1api1storage.blob.core.windows.net/faces/';
-// const person_group_id = uuid();
-
+//************* */
 // using the following function to wait for the training of the PersonGroup to complete.
 const sleep = (ms) => {
   return new Promise((resolve) => setTimeout(resolve, ms));
 };
 
+// const person_group_id = uuid();
 
-DetectFaceExtract(client, image_base_url);
+
+DetectFaceExtract(client, IMAGE_BASE_URL);
