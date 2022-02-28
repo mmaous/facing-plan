7ffa1397718438ead8824b934f4711cff56c6f59
Mhand <amd.maous@gmail.com>
2022-02-28 14:00:12 +0100

    Separated the modules

---
+++ client.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const microsoftRest = require('@azure/ms-rest-js');
+const Face = require('@azure/cognitiveservices-face');
+
+//load the .env file params in process.env
+require('dotenv').config();
+
+const KEY = process.env.KEY;
+const ENDPOINT = process.env.ENDPOINT;
+
+// credentials
+const credentials = new microsoftRest.ApiKeyCredentials({
+  inHeader: { 'Ocp-Apim-Subscription-key': KEY },
+});
+
+const client = new Face.FaceClient(credentials, ENDPOINT);
+
+module.exports = client;
--- index.js
+++ index.js
@@ -1,23 +1,14 @@
 'use strict';
 
-const microsoftRest = require('@azure/ms-rest-js');
-const Face = require('@azure/cognitiveservices-face');
 const uuid = require('uuid/v4');
-
-//load the .env file params in process.env
-require('dotenv').config();
-
-const KEY = process.env.KEY;
-const ENDPOINT = process.env.ENDPOINT;
-
-// credentials
-const credentials = new microsoftRest.ApiKeyCredentials({
-  inHeader: { 'Ocp-Apim-Subscription-key': KEY },
-});
-
-const client = new Face.FaceClient(credentials, ENDPOINT);
+const client = require('./client');
 
 // global values and helper functions
 const image_base_url =
   'https://csdx.blob.core.windows.net/resources/Face/Images/';
 const person_group_id = uuid();
+
+// using the following function to wait for the training of the PersonGroup to complete.
+const sleep = (ms) => {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+};
